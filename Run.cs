992fc4ae6636ba301c0bce216b674098cec913cc
Heber Holanda <heberpcl@gmail.com>
2019-11-02 10:10:54 -0300

    Init

---
+++ Run.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyBackup_Tool
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
